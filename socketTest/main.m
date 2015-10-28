124444efad147aff52a8324366e684362518a0e1
imac <Damon@imacdeimac.local>
2015-10-28 09:47:07 +0800

    Initial Commit

---
+++ socketTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  socketTest
+//
+//  Created by Damon on 15/10/28.
+//  Copyright © 2015年 zhangle.in.iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
